72dd4aa07349ac3d4782f1311d14507b6b8b4ebc
vprix21 <vkalwale21@gmail.com>
2026-03-29 12:18:49 +0530

    initial commit

---
+++ components/blur-text/BlurText.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function BlurText() {
+  return (
+    <p className="text-5xl font-semibold text-white transition duration-300 blur-sm hover:blur-none">
+      NeonBlade UI
+    </p>
+  )
+}
---
+++ components/red-text/RedText.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function RedText() {
+  return (
+    <p className="text-5xl font-semibold text-red-500">
+      NeonBlade UI RED
+    </p>
+  )
+}
